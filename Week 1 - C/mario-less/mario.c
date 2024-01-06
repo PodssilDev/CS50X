bb88011074e84d1a86b4d7484c21714bddb6ff27
PodssilDev <john.serrano@usach.cl>
2024-01-06 02:05:48 -0300

    Week 1 - Problem Set 1: Hello and Mario

---
+++ Week 1 - C/mario-less/mario.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Mario - less comfortable
+// Problem Set 1 - Week 1 - C
+// Author: John Serrano
+
+int main(void)
+{
+    int height; // define height for user input
+
+    do // do loop to only get numbers highter than 1 inclusive
+    {
+        height = get_int("Height: ");
+    } while (height < 1);
+
+    int spaces = height - 1; // number of spaces to print
+    int blocks = 1;          // number of "blocks" to print
+
+    while (spaces >= 0) // while spaces aren't less than 0 (piramid completed)
+    {
+        for (int i = 0; i < spaces; i++)
+        {
+            printf(" "); // print the left side spaces
+        }
+        for (int j = 0; j < blocks; j++)
+        {
+            printf("#"); // print the "blocks"
+        }
+        printf("\n"); // print the newline so piramid can grow
+
+        blocks += 1; // blocks increase with each new line
+        spaces -= 1; // spaces decrease with each new line
+    }
+}
